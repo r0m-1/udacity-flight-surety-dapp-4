c14b597991a9984947d237bc372484483a9a373b
b3nk3i <rom1.petit@gmail.com>
2023-01-03 23:55:56 +0100

    use callback to handle events after all oracles are registered. add submit random oracle responses

--- src/server/server.js
+++ src/server/server.js
@@ -13,40 +13,93 @@ web3.eth.defaultAccount = accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
 
 const TEST_ORACLES_COUNT = 20;
-let oracles = [];
 
+function registerOracles(index, fee, callback) {
 
-function registerOracles(index, fee) {
-
-    console.log(`register oracle[${index}] @ ${accounts[index]} for ${fee}`);
+    //console.log(`register oracle[${index}] @ ${accounts[index]} for ${fee}`);
 
     flightSuretyApp.methods.registerOracle()
         .send({from: accounts[index], value: fee, gas: 4500000}, (error, result) => {
             console.log(`oracle[${index}] registered`);
 
             if (index < TEST_ORACLES_COUNT) {
-                registerOracles(index + 1, fee);
+                registerOracles(index + 1, fee, callback);
+            } else {
+                console.log(`${index} oracle registered`);
+                callback();
             }
         });
+}
+
+function submitRandomStatus(accountIdx, index, airline, flight, timestamp) {
+
+    console.log(`submitRandomStatus ${accountIdx} ${index}, ${airline}, ${flight}, ${timestamp}`);
+
+    if (accountIdx < TEST_ORACLES_COUNT) {
+        flightSuretyApp.methods
+            .getMyIndexes()
+            .call({from: accounts[accountIdx], gas: 100000})
+            .then(indices => {
+                console.log(indices);
+
+                if (indices[0] === index || indices[1] === index || indices[2] === index) {
+
+                    let statusCode = randomStatus();
+
+                    //console.log(`submitOracleResponse ${index}, ${airline}, ${flight}, ${timestamp}, ${statusCode}`);
+
+                    flightSuretyApp.methods.submitOracleResponse(index, airline, flight, timestamp, statusCode)
+                        .call({from: accounts[accountIdx], gas: 100000})
+                        .then(() => {
+                            console.log(`OracleResponseSubmitted ${index}, ${airline}, ${flight}, ${timestamp}, ${statusCode}`);
+                            submitRandomStatus(accountIdx + 1, index, airline, flight, timestamp);
+                        });
+                } else {
+                    submitRandomStatus(accountIdx + 1, index, airline, flight, timestamp);
+                }
+            });
+    }
+}
+
+function randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+/**
+ * generate a random status
+ * STATUS_CODE_UNKNOWN = 0;
+ * STATUS_CODE_ON_TIME = 10;
+ * STATUS_CODE_LATE_AIRLINE = 20;
+ * STATUS_CODE_LATE_WEATHER = 30;
+ * STATUS_CODE_LATE_TECHNICAL = 40;
+ * STATUS_CODE_LATE_OTHER = 50;
+ */
+function randomStatus() {
+    return randomIntFromInterval(0, 5) * 10;
+}
+
+function handleEvents() {
+    flightSuretyApp.events.OracleRequest({
+        fromBlock: 0
+    }, function (error, event) {
+        if (error) {
+            console.log(error);
+        }
+        //console.log(event);
+        console.log(`OracleRequest(index=${event.returnValues.index}, airline=${event.returnValues.airline}, flight=${event.returnValues.flight} timestamp=${event.returnValues.timestamp})`)
+
+        submitRandomStatus(1, event.returnValues.index, event.returnValues.airline, event.returnValues.flight, event.returnValues.timestamp);
+
+    });
 }
 
 flightSuretyApp.methods.REGISTRATION_FEE().call().then(async fee => {
 
     console.log(`REGISTRATION_FEE: ${fee}`);
 
-    registerOracles(1, fee);
+    registerOracles(1, fee, handleEvents);
 });
 
-
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-}, function (error, event) {
-    if (error) {
-        console.log(error);
-    }
-    //console.log(event);
-    console.log(`OracleRequest(index=${event.returnValues.index}, airline=${event.returnValues.airline}, flight=${event.returnValues.flight} timestamp=${event.returnValues.timestamp})`)
-});
 
 const app = express();
 app.get('/api', (req, res) => {
