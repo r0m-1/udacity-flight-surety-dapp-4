93bff25c9821a914b3d078e7c84bc67708a90795
b3nk3i <rom1.petit@gmail.com>
2023-01-03 17:05:04 +0100

    add callback based registerOracles

--- src/server/server.js
+++ src/server/server.js
@@ -6,15 +6,46 @@ import express from 'express';
 
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
-web3.eth.defaultAccount = web3.eth.accounts[0];
+
+const accounts = await web3.eth.getAccounts();
+web3.eth.defaultAccount = accounts[0];
+
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+
+const TEST_ORACLES_COUNT = 20;
+let oracles = [];
+
+
+function registerOracles(index, fee) {
+
+    console.log(`register oracle[${index}] @ ${accounts[index]} for ${fee}`);
+
+    flightSuretyApp.methods.registerOracle()
+        .send({from: accounts[index], value: fee, gas: 4500000}, (error, result) => {
+            console.log(`oracle[${index}] registered`);
+
+            if (index < TEST_ORACLES_COUNT) {
+                registerOracles(index + 1, fee);
+            }
+        });
+}
+
+flightSuretyApp.methods.REGISTRATION_FEE().call().then(async fee => {
+
+    console.log(`REGISTRATION_FEE: ${fee}`);
+
+    registerOracles(1, fee);
+});
 
 
 flightSuretyApp.events.OracleRequest({
     fromBlock: 0
 }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
+    if (error) {
+        console.log(error);
+    }
+    //console.log(event);
+    console.log(`OracleRequest(index=${event.returnValues.index}, airline=${event.returnValues.airline}, flight=${event.returnValues.flight} timestamp=${event.returnValues.timestamp})`)
 });
 
 const app = express();
